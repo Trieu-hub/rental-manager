6015c5a3fc4143c3fb23b3bd632bac21f6ef2ed1
nguyenquoctrieu9a52006@gmail.com <nguyenquoctrieu9a52006@gmail.com>
2025-08-10 22:09:19 +0700

    Bigger changes

--- RentalManager.java
+++ RentalManager.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
+
 public class RentalManager {
     public static void main(String[] args) {
-        Room room1 = new Room(001, "1", 1500);
-        System.out.println(room1.getInfo());
+        // Hàm nhập
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Số phòng bạn đang ở: ");
+        int roomNum = sc.nextInt();
+        System.out.print("Số người phòng bạn đang có: ");
+        int roomLives = sc.nextInt();
+        System.out.print("Số tiền thuê phòng của bạn là: ");
+        double roomRent = sc.nextDouble();
+
+        // khởi tạo constructor
+        Room room = new Room(roomNum, roomLives, roomRent);
+        room.Lives();
+
+        // gọi hàm lấy thông tin
+        System.out.println(room.getInfo());
+
+        sc.close();
     }
 }
--- Room.java
+++ Room.java
@@ -1,15 +1,23 @@
 public class Room {
     private int num;
-    private String n;
+    private int n;
     private double rent;
 
-    public Room(int num, String n, double rent){
+    public Room(int num, int n, double rent){
         this.num=num;
-        this.n=n;
+        this.n=Math.abs(n);
         this.rent=rent;
     }
 
+    public void Lives(){
+        if(n==1)
+            System.out.println("1 person");
+        else{
+            System.out.println(n +"persons");
+        }
+    }
+
     public String getInfo(){
-        return "Room: " + num + " have " + n + "person" + " Rent: " + rent;
+        return "Room: " + num + " has " + n + " person(s)." + " Rent: " + rent;
     }
 }
